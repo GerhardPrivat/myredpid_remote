b3f9375407c3fe095904f584dbb68de86229bf84
Gerhard <Gerhard.Schunk@gmail.com>
2017-04-19 12:23:53 +0200

    oszi mode
    read two traces in an infinite loop

--- myredpid_remote_main.py
+++ myredpid_remote_main.py
@@ -15,9 +15,10 @@ import pdb
 import redpitaya_scpi as scpi
 import time as tm
 from matplotlib.pyplot import cm 
+from itertools import cycle
+from redpid_lib import 
 
 #############PRE SETTINGS
-ini_time_ms = tm.time()
 pathname = os.getcwd()
 data_path = pathname + '\\data'
 plots_path = pathname + '\\plots'
@@ -37,6 +38,7 @@ ticks_font = {'size':'11'}
 label_font = {'size':'11'}
 legend_font = {'size':'8'}
 
+plt.clf()
 fig1 = plt.figure(1,tight_layout=True,figsize=(22.5/2.53, 10./2.53))
 ax1 = fig1.add_subplot(211)
 ax2 = fig1.add_subplot(212)
@@ -44,7 +46,6 @@ fig1.subplots_adjust(left=0.25)
 fig1.subplots_adjust(bottom=0.25)
 fig1.subplots_adjust(top=0.90)
 fig1.subplots_adjust(right=0.95)
-plt.cla()
 
 for axis in ['top','bottom','left','right']:
 	ax1.spines[axis].set_linewidth(1.)
@@ -52,7 +53,7 @@ for axis in ['top','bottom','left','right']:
 
 plt_title = ax1.set_title(' ')
 
-color=iter(cm.rainbow(np.linspace(0,1,10)))
+color=cycle(cm.rainbow(np.linspace(0,1,20)))
 
 ax1_tace1, = ax1.plot([],[])
 ax2_tace2, = ax2.plot([],[])
@@ -78,33 +79,40 @@ rp_s = scpi.scpi("10.64.11.12")
 #    print ("Measured voltage on AI["+str(i)+"] = "+str(amp_V)+"V")
 
 #Decimation,Sampling Rate,Time scale/length of a buffer,Trigger delay in samples,Trigger delay in seconds
-#1,125 MS/s,131.072 us,from - 8192 to x,-6.554E-5 to x
-#8,15.6 MS/s,1.049 ms,from - 8192 to x,-5.243E-4 to x
-#64,1.9 MS/s,8.389 ms,from - 8192 to x,-4.194E-3 to x
-#1024=10**10,122.0 MS/s,134.218 ms,from - 8192 to x,-6.711E-2 to x
-#8192=10**13,15.2 kS/s,1.074 s,from - 8192 to x,-5.369E-1 to x
-#65536,7.6 kS/s,8.590 s,from - 8192 to x,-4.295E+0 to x
+#1,125 MS/s                  ,131.072 us,from - 8192 to x,-6.554E-5 to x
+#8,15.6 MS/s                ,1.049 ms,from - 8192 to x,-5.243E-4 to x
+#64 = 2** 6 ,1.9 MS/s                 ,8.389 ms,from - 8192 to x,-4.194E-3 to x
+#1024=2**10,122.0 MS/s 	,134.218 ms,from - 8192 to x,-6.711E-2 to x
+#8192=2**13,15.2 kS/s 	      ,1.074 s,from - 8192 to x,-5.369E-1 to x
+#65536,7.6 kS/s              ,8.590 s,from - 8192 to x,-4.295E+0 to x
 
 #decimation = int(2**10) # max around 134 ms
 #decimation = int(2**7) # max around 16 ms -> strange modes
-decimation = int(2**6) # max around 8.389 ms
-
-print 'start recording'
-print 'decimation is', decimation
-
+decimation = int(2**6) # max around 8.389 ms for 2**14 = 16384 pts
+buff_len = 2**13
+#buff_len = 2**4
+num_run = 10
 sample_rate_MHz = 125. / decimation
 delta_time_ms_ms = 1. / sample_rate_MHz * 10**(-3) 
-print 'sample_rate_MHz', sample_rate_MHz, ' sampling distance_ms', delta_time_ms_ms
+
+print '###START RECORDING###'
+print 'decimation is', decimation, 
+print 'Nr of runs', num_run
+print 'Nr of samples', buff_len
+print 'sample_rate_MHz', sample_rate_MHz
+print 'sampling distance_ms', delta_time_ms_ms
 
 ###START RECORDING
 rp_s.tx_txt('ACQ:RST')
 rp_s.tx_txt('ACQ:DEC '+str(decimation))
 rp_s.tx_txt('ACQ:TRIG:LEV 1')
 
-
-for ite_meas in range(5):
+ini_time_ms = tm.time()
+#for ite_meas in range(num_run):
+while True:
+	ite_meas = 1
 	start_time_ms = tm.time()
-	rp_s.tx_txt('ACQ:TRIG:DLY ' + str(int(2**13-1000.)))
+	rp_s.tx_txt('ACQ:TRIG:DLY ' + str(int(buff_len-1000.)))
 	rp_s.tx_txt('ACQ:START')
 #	tm.sleep(1.)	#pause to refresh buffer
 	rp_s.tx_txt('ACQ:TRIG EXT_PE')
@@ -118,28 +126,29 @@ for ite_meas in range(5):
 	        break
 								
 	###READ TACE 1
-	rp_s.tx_txt('ACQ:SOUR1:DATA?')
+#	rp_s.tx_txt('ACQ:SOUR1:DATA?')
+	rp_s.tx_txt('ACQ:SOUR1:DATA:OLD:N? ' + str(int(buff_len)))
 	buff_string = rp_s.rx_txt()
 	buff_string = buff_string.strip('{}\n\r').replace("  ", "").split(',')
 	buff = list(map(float, buff_string))
 	y_trace1_V = np.asarray(buff)
+	time_trace1_ms = range(len(y_trace1_V))
+	time_trace1_ms = np.asarray(time_trace1_ms) * delta_time_ms_ms
 
 	###READ TACE 2
-	rp_s.tx_txt('ACQ:SOUR2:DATA?')
+#	rp_s.tx_txt('ACQ:SOUR2:DATA?')
+	rp_s.tx_txt('ACQ:SOUR2:DATA:OLD:N? ' + str(int(buff_len)))
 	buff_string = rp_s.rx_txt()
 	buff_string = buff_string.strip('{}\n\r').replace("  ", "").split(',')
 	buff = list(map(float, buff_string))
 	y_trace2_V = np.asarray(buff)
-	
-#	print y_trace1_V
-#	pdb.set_trace()
-	#evalutae data
-	
+	time_trace2_ms = range(len(y_trace2_V))
+	time_trace2_ms = np.asarray(time_trace2_ms) * delta_time_ms_ms
+
 	###PROCESS DATA
 	y_trace1_V = y_trace1_V- np.average(y_trace1_V)
 	y_trace2_V = y_trace2_V- np.average(y_trace2_V)
 	
-	time_ms = np.arange(0,len(y_trace1_V)*delta_time_ms_ms,delta_time_ms_ms)
 
 	stop_time_ms = tm.time()
 	
@@ -149,28 +158,31 @@ for ite_meas in range(5):
 	print '\nNew trace:'
 	print 'Relative start time', rel_start_time, ' ms'
 	print 'run time', run_time_ms, ' ms'
+			
+	if 1==1:
+		###PLOT TRACES	
+		plt.ion()		
+		c=next(color)
 		
-	###PLOT TRACES	
-	plt.ion()		
-	c=next(color)
+		plt_title = ax1.set_title('measured after ' + str(rel_start_time) + ' ms',**title_font)
+		
+		ax1_tace1.remove()
+		ax1_tace1, = ax1.plot(time_trace1_ms,y_trace1_V,markersize=5,linewidth = 1,color=c)
+		ax1.set_xlim([0.,max(time_trace1_ms)])
 	
-	plt_title = ax1.set_title('measured after ' + str(rel_start_time) + ' ms',**title_font)
-	
-	ax1_tace1.remove()
-	ax1_tace1, = ax1.plot(time_ms,y_trace1_V,markersize=5,linewidth = 1,color=c)
-	ax1.set_xlim([0.,max(time_ms)])
-
-	ax2_tace2.remove()
-	ax2_tace2, = ax2.plot(time_ms,y_trace2_V,markersize=5,linewidth = 1,color=c)
-	ax2.set_xlim([0.,max(time_ms)])
-	
-	plt.draw()
-	
+		ax2_tace2.remove()
+		ax2_tace2, = ax2.plot(time_trace2_ms,y_trace2_V,markersize=5,linewidth = 1,color=c)
+		ax2.set_xlim([0.,max(time_trace2_ms)])
+		
+		plt.draw()
+		
 	#SAVE DATA
 	savename = str(ite_meas) + '_test'
-	np.array(y_trace1_V).dump(open(data_path+'\\'+savename+'.npy', 'wb'))
+#	np.array(y_trace1_V).dump(open(data_path+'\\'+savename+'.npy', 'wb'))
 	#myArray = np.load(open('array.npy', 'rb'))
 
+av_time = int(1000.*(stop_time_ms-ini_time_ms ) / num_run)
+print 'averaged run time is ', av_time, ' ms'
 ###LABELS AND LEGENDS
 
 #TICKS
---
+++ redpid_lib.py
@@ -0,0 +1,326 @@
+#!/usr/bin/env mpython_q
+import numpy as np
+import sys
+from numpy import NaN, Inf, arange, isscalar, array, asarray
+import ntpath
+
+def int_to_roman(input):
+	"""
+	Convert an integer to Roman numerals.
+	"""
+	if type(input) != type(1):
+		raise TypeError, "expected integer, got %s" % type(input)
+	if not 0 < input < 4000:
+		raise ValueError, "Argument must be between 1 and 3999"   
+	ints = (1000, 900,  500, 400, 100,  90, 50,  40, 10,  9,   5,  4,   1)
+	nums = ('M',  'CM', 'D', 'CD','C', 'XC','L','XL','X','IX','V','IV','I')
+	result = ""
+	for i in range(len(ints)):
+		count = int(input / ints[i])
+		result += nums[i] * count
+		input -= ints[i] * count
+	return result
+    
+def ntest(lda,T,pol=0,MgO=0,MgO_th=5.0,E = 0): return 1.5
+
+def path_leaf(path):
+    head, tail = ntpath.split(path)
+    tail =  ('.').join(tail.split('.')[:-1])
+    return tail or ntpath.basename(head)
+    
+def tiltcorrect(ydata,windowlen=100,degree = 1):
+#    import poly_iter
+    x_ground = np.arange(len(ydata))
+    x_ground_fit = smooth(x_ground,window_len=int(len(x_ground)/windowlen),window='hanning')
+    ydata_fit    = smooth(ydata   ,window_len=int(len(ydata)   /windowlen),window='hanning')
+    coeff = np.polyfit(x_ground_fit,ydata_fit, degree)
+    
+#    if degree == 1:
+#        ytilt = tuple(x*coeff[0] + coeff[1] for x in x_ground)
+#    if degree == 2:
+#        ytilt = tuple(x*x*coeff[0] + x*coeff[1] + coeff[2]  for x in x_ground)
+#    if degree == 3:
+#        ytilt = tuple(x*x*x*coeff[0] + x*x*coeff[1] + x*coeff[2] + coeff[3]  for x in x_ground)
+    ytilt = np.array([], dtype='double')
+    for xx in x_ground:
+        ytilt = np.append(ytilt,poly_horner(np.flipud(coeff),xx))
+    return  ydata - ytilt 
+
+def poly_horner(A, x):
+    p = A[-1]
+    i = len(A) - 2
+    while i >= 0:
+        p = p * x + A[i]
+        i -= 1
+    return p
+    
+def poly_naive(x, coeff):
+    result = coeff[0]
+    for i in range(1, len(coeff)):
+        result = result + coeff[i] * x**i
+    return result
+    
+def poly_iter(A, x):
+    p = 0
+    xn = 1
+    for a in A:
+        p += xn * a
+        xn *= x
+    return p
+
+
+def smooth(x,window_len=11,window='hanning'):
+    """smooth the data using a window with requested size.
+    
+    http://wiki.scipy.org/Cookbook/SignalSmooth
+    
+    This method is based on the convolution of a scaled window with the signal.
+    The signal is prepared by introducing reflected copies of the signal 
+    (with the window size) in both ends so that transient parts are minimized
+    in the begining and end part of the output signal.
+    
+    input:
+        x: the input signal 
+        window_len: the dimension of the smoothing window; should be an odd integer
+        window: the type of window from 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'
+            flat window will produce a moving average smoothing.
+
+    output:
+        the smoothed signal
+        
+    example:
+
+    t=linspace(-2,2,0.1)
+    x=sin(t)+randn(len(t))*0.1
+    y=smooth(x)
+    
+    see also: 
+    
+    np.hanning, np.hamming, np.bartlett, np.blackman, np.convolve
+    scipy.signal.lfilter
+ 
+    TODO: the window parameter could be the window itself if an array instead of a string
+    NOTE: length(output) != length(input), to correct this: return y[(window_len/2-1):-(window_len/2)] instead of just y.
+    """
+
+    if x.ndim != 1:
+        raise ValueError, "smooth only accepts 1 dimension arrays."
+
+    if x.size < window_len:
+        raise ValueError, "Input vector needs to be bigger than window size."
+
+
+    if window_len<3:
+        return x
+
+
+    if not window in ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']:
+        raise ValueError, "Window is on of 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'"
+
+
+    s=np.r_[x[window_len-1:0:-1],x,x[-1:-window_len:-1]]
+    #print(len(s))
+    if window == 'flat': #moving average
+        w=np.ones(window_len,'d')
+    else:
+        w=eval('np.'+window+'(window_len)')
+
+    y=np.convolve(w/w.sum(),s,mode='valid')
+    return y[(window_len/2-1):-(window_len/2)]
+    
+
+
+def peakdet(v, delta, x = None):
+    """
+    Converted from MATLAB script at http://billauer.co.il/peakdet.html
+    https://gist.github.com/endolith/250860
+    Returns two arrays
+    
+    function [maxtab, mintab]=peakdet(v, delta, x)
+    %PEAKDET Detect peaks in a vector
+    %        [MAXTAB, MINTAB] = PEAKDET(V, DELTA) finds the local
+    %        maxima and minima ("peaks") in the vector V.
+    %        MAXTAB and MINTAB consists of two columns. Column 1
+    %        contains indices in V, and column 2 the found values.
+    %      
+    %        With [MAXTAB, MINTAB] = PEAKDET(V, DELTA, X) the indices
+    %        in MAXTAB and MINTAB are replaced with the corresponding
+    %        X-values.
+    %
+    %        A point is considered a maximum peak if it has the maximal
+    %        value, and was preceded (to the left) by a value lower by
+    %        DELTA.
+    
+    % Eli Billauer, 3.4.05 (Explicitly not copyrighted).
+    % This function is released to the public domain; Any use is allowed.
+    
+    """
+    maxtab = []
+    mintab = []
+       
+    if x is None:
+        x = arange(len(v))
+    
+    v = asarray(v)
+    
+    if len(v) != len(x):
+        sys.exit('Input vectors v and x must have same length')
+    
+    if not isscalar(delta):
+        sys.exit('Input argument delta must be a scalar')
+    
+    if delta <= 0:
+        sys.exit('Input argument delta must be positive')
+    
+    mn, mx = Inf, -Inf
+    mnpos, mxpos = NaN, NaN
+    
+    lookformax = True
+    
+    for i in arange(len(v)):
+        this = v[i]
+        if this > mx:
+            mx = this
+            mxpos = x[i]
+        if this < mn:
+            mn = this
+            mnpos = x[i]
+        
+        if lookformax:
+            if this < mx-delta:
+                maxtab.append((mxpos, mx))
+                mn = this
+                mnpos = x[i]
+                lookformax = False
+        else:
+            if this > mn+delta:
+                mintab.append((mnpos, mn))
+                mx = this
+                mxpos = x[i]
+                lookformax = True
+
+    return array(maxtab), array(mintab)
+
+def nu2lambda(x):
+    #for example: lambda in nm to frequency in GHz
+    cms = 299792458 # speed of light
+    return cms/x
+    
+def lambda2nu(x):
+    #for example: lambda in nm to frequency in GHz
+    cms = 299792458 # speed of light
+    return cms/x
+  
+def double_exponential(t=range(1,100),t0=0,t_d=1,amp=1,y0=1):
+    import math
+    return amp * math.exp(-abs((t - t0) / t_d)) + y0
+				
+def lorentz(x=range(1,100),x0=0,gamma=1,amp=1,y0=1):
+    return amp / (1.0 + 4*((x - x0) / gamma)**2 ) + y0
+    
+def gauss(x, *p):
+    A, mu, sigma, y0 = p
+    return A*np.exp(-(x-mu)**2/(2.*sigma**2)) + y0
+    
+def lowpass(cutoff, fs, order=5):
+    import numpy as np
+    from scipy.signal import butter, lfilter, freqz
+    import matplotlib.pyplot as plt
+    
+    def butter_lowpass(cutoff, fs, order=5):
+        nyq = 0.5 * fs
+        normal_cutoff = cutoff / nyq
+        b, a = butter(order, normal_cutoff, btype='low', analog=False)
+        return b, a
+    def butter_lowpass_filter(data, cutoff, fs, order=5):
+        b, a = butter_lowpass(cutoff, fs, order=order)
+        y = lfilter(b, a, data)
+        return y
+
+    # Filter requirements.
+    order = 6
+    fs = 30.0       # sample rate, Hz
+    cutoff = 3.667  # desired cutoff frequency of the filter, Hz
+    
+    # Get the filter coefficients so we can check its frequency response.
+    b, a = butter_lowpass(cutoff, fs, order)
+    
+    # Plot the frequency response.
+    w, h = freqz(b, a, worN=8000)
+    plt.subplot(2, 1, 1)
+    plt.plot(0.5*fs*w/np.pi, np.abs(h), 'b')
+    plt.plot(cutoff, 0.5*np.sqrt(2), 'ko')
+    plt.axvline(cutoff, color='k')
+    plt.xlim(0, 0.5*fs)
+    plt.title("Lowpass Filter Frequency Response")
+    plt.xlabel('Frequency [Hz]')
+    plt.grid()
+
+def read_data(filename,delim = ' '):
+# Funktion zum Einlesen der selbsterzeugten kalibrationsdatein
+    import csv
+    rawdata=[]
+	
+    reader = csv.reader(open(filename, 'r'), delimiter=delim)#, quotechar='|')
+
+    for line in reader:
+		#print len(line)	
+          rawdata.append(line)
+	#ardata = np.array(rawdata, dtype='float64')
+    return rawdata#ardata
+    
+def read_agilent_DSOx2024a(expdatfolder,expfilenr=0,expfiletype='csv',grdsubtract=False):
+#This function read a measured frequeny spectrum and prepares it a bit  
+#
+#@author: Gerhard Schunk  
+    from Ghad_lib import read_data, smooth, path_leaf
+    import numpy as np
+    import csv
+    import glob
+    
+    headerlines = 4
+    x1 = np.array([], dtype='float64')
+    y1 = np.array([], dtype='float64')
+    y2 = np.array([], dtype='float64')
+    y3 = np.array([], dtype='float64')
+    y4 = np.array([], dtype='float64')
+    
+    if expfiletype=='csv':
+        expfilepaths = glob.glob(expdatfolder + '//*.csv')
+        expfilename =  path_leaf(expfilepaths[expfilenr])   
+        reader = csv.reader(open(expfilepaths[expfilenr], 'r'), delimiter=',')#, quotechar='|')
+        
+        
+        line_count = 0
+        for line in reader:
+            if headerlines > line_count:
+                line_count = line_count + 1
+                continue
+            x1 = np.append(x1,float(line[0]))
+            y1 = np.append(y1,float(line[1]))
+            y2 = np.append(y2,float(line[2]))
+            y3 = np.append(y3,float(line[3]))
+            y4 = np.append(y4,float(line[4]))
+            
+    
+    print 'exp data name: ', expfilename, '\n'
+
+    if grdsubtract:
+        groundsmoothlen = int(round(len(yfreq)/10))
+        yfreq_ground = smooth(yfreq,groundsmoothlen,window='hanning')
+        yfreq_ground = yfreq_ground[0:len(yfreq)]
+        yfreq_filter = yfreq - yfreq_ground
+        for ite in range(yfreq_filter.size):
+            if yfreq_filter[ite]<0:
+                yfreq_filter[ite] = 0
+                
+    return  x1, y1, y2, y3, y4, expfilename              
+#    return xfreq_GHz, yfreq, expfilename
+                
+def autocorr(x):
+#    result = np.convolve(x, x, mode='same')
+    result = np.convolve(x, x, mode='full')
+    return result[result.size/2.:]
+#    return result
+                
+                
